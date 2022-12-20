209e53bb75f23fb26ec1d13c2fa22c68ec57cbe4
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2022-12-20 15:42:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,13 @@ if vAR_selected=="Sorting order":
 if vAR_selected=="OddEven, Quotient and Remainder":
     vAR_st.markdown("<p font='IBM Flex Sans'; style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find ODD or EVEN, Quotient and Remainder</p>", unsafe_allow_html=True)
 
-   
+#for background color of sidebar
+vAR_st.markdown("""<style>.css-1d391kg, .e1fqkh3o1 {
+    background-color: #4c85e4;
+    width: 19rem;
+
+
+
 #for horizontal line
 vAR_st.markdown("""
 <hr style="width:100%;height:3px;background-color:gray;border-width:10">
